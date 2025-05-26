3e877226d85cbf55a4e9813201c1faf1d68f7190
mahdibataie <mehdi.br80@gmail.com>
2025-05-26 19:29:07 +0330

    updated and solved problem

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
+//import { environment } from './environments/environment';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
@@ -10,9 +10,9 @@ import { FormsModule } from '@angular/forms';
 
 import { routes } from './app/app-routing.module';
 
-if (environment.production) {
-  enableProdMode();
-}
+//if (environment.production) {
+  //enableProdMode();
+//}
 
 bootstrapApplication(AppComponent, {
   providers: [
